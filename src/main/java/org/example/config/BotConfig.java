06bb3960687d27c9d8599fe229d2fe3e2bfb8591
Kirill Ulyanov <ulyanow-kirill@mail.ru>
2026-03-16 07:42:01 +0300

    Update BOT_TOKEN to use placeholder value

--- src/main/java/org/example/config/BotConfig.java
+++ src/main/java/org/example/config/BotConfig.java
@@ -3,6 +3,6 @@ package org.example.config;
 public class BotConfig
 {
     public static final String BOT_USERNAME = "JL5";
-    public static final String BOT_TOKEN = "8771382276:AAEhzlFsfFcp5xhN-N_cNzQpwMDp4Tihq-g";
+    public static final String BOT_TOKEN = "YOUR_TOKEN_HERE";
     public static final String RESOURCES_PATH = "src/main/resources/";
 }
